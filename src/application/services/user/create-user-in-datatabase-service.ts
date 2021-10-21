829c307509163339305e09c0d96fc181aff3cc6d
isaquetdiniz <itd@cin.ufpe.br>
2021-10-20 21:39:54 -0300

    feat: create application layer

---
+++ src/application/services/user/create-user-in-datatabase-service.ts
@@ -0,0 +1,36 @@
+import { CreateUserUsecase } from '@/domain/usecases/user'
+import { CreateUserInDatabaseRepository } from '@/application/protocols/database/repositories/user'
+import { UUIDGenerator } from '@/application/protocols/database/utils'
+import { User } from '@/domain/entities'
+
+type CreateUserInDatabaseServiceInjectables = {
+    createUserInDatabaseRepository: CreateUserInDatabaseRepository,
+    UUIDGenerator: UUIDGenerator
+}
+
+class CreateUserInDatabaseService implements CreateUserUsecase {
+  private readonly createUserInDatabaseRepository: CreateUserInDatabaseRepository
+  private readonly UUIDGenerator: UUIDGenerator
+
+  constructor ({
+    createUserInDatabaseRepository,
+    UUIDGenerator
+  }: CreateUserInDatabaseServiceInjectables) {
+    this.createUserInDatabaseRepository = createUserInDatabaseRepository
+    this.UUIDGenerator = UUIDGenerator
+  }
+
+  async create (userParams: CreateUserUsecase.Params): Promise<CreateUserUsecase.Result> {
+    const { isAdmin, name, email, profileImageUrl } = userParams
+
+    const id = await this.UUIDGenerator.generate()
+
+    const newUser = new User({ id, isAdmin, name, email, profileImageUrl })
+
+    await this.createUserInDatabaseRepository.createUser(newUser)
+
+    return newUser
+  }
+}
+
+export { CreateUserInDatabaseService }
