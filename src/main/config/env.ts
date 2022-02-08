8203902907bc77092784f831abe4d8631aacc303
isaquetdiniz <itd@cin.ufpe.br>
2022-02-08 01:13:23 -0300

    chore: configure new way of docker

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -19,7 +19,7 @@ export const env = {
   },
   databases: {
     postgres: {
-      url: process.env.DATABASE_URL as string,
+      url: `postgresql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}?schema=public`,
     },
   },
   logs: {
