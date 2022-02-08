97f421cbf867e3be12e187599b536bee72554747
isaquetdiniz <itd@cin.ufpe.br>
2022-02-08 15:32:54 -0300

    chore: refactor docker flow

--- src/main/infra/express/config/app.ts
+++ src/main/infra/express/config/app.ts
@@ -2,17 +2,20 @@ import express from 'express';
 
 import setupMiddlewares from './middlewares';
 import setupRoutes from './routes';
-import setupSwagger from './swagger';
 
 import { env } from '@/main/config';
 
 const app = express();
 
-if (env.application.mode !== 'production') {
-  setupSwagger(app);
-}
+(async () => {
+  if (env.application.mode !== 'production') {
+    await (async () => {
+      (await import('./swagger.js')).default(app);
+    })();
+  }
 
-setupMiddlewares(app);
-setupRoutes(app);
+  setupMiddlewares(app);
+  setupRoutes(app);
+})();
 
 export default app;
