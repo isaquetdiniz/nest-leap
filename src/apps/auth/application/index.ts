896881608c6254d2e73d4b76b42218f76c535b41
argas7 <argas@cin.ufpe.br>
2022-10-13 17:10:03 -0300

    refactor: prisma auth_user repositories

---
+++ src/apps/auth/application/index.ts
@@ -0,0 +1,9 @@
+export * from './exceptions/auth_user_already_made_first_login.exception';
+export * from './exceptions/auth_user_need_set_password.exception';
+export * from './exceptions/auth_user_not_found_by_token.exception';
+export * from './exceptions/auth_user_not_found.exception';
+export * from './exceptions/auth_user_not_made_first_login.exception';
+
+export * from './repos/auth_user.repository';
+
+export * from './services/auth_user_cloud.service';
--- src/apps/auth/infra/prisma/repositories/prisma_auth_user.repository.ts
+++ src/apps/auth/infra/prisma/repositories/prisma_auth_user.repository.ts
@@ -1,18 +1,18 @@
 import { PrismaClient } from '@prisma/client';
+
 import { prismaConnector, PrismaException } from '@/shared/infra/prisma';
 
-import { IGetAuthUserByEmailRepository } from '@/domains/auth/usecases/repos';
+import { IAuthUserRepository } from '@/apps/auth/application';
+import { AuthUser } from '@/apps/auth/domain';
 
-export class PrismaGetAuthUserByEmailRepository
-  implements IGetAuthUserByEmailRepository
-{
+export class PrismaAuthUserRepository implements IAuthUserRepository {
   private prismaConnection: PrismaClient;
 
   constructor() {
     this.prismaConnection = prismaConnector.connect();
   }
 
-  async get(email: string): IGetAuthUserByEmailRepository.Result {
+  async getByEmail(email: string): Promise<AuthUser> {
     try {
       const [user] = await this.prismaConnection.user.findMany({
         where: { email, enabled: true },
