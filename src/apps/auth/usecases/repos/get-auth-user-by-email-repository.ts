14fa24f0de4e7f3c1eb1aa03a7286b3e984fdda9
argas7 <argas@cin.ufpe.br>
2022-10-13 17:06:36 -0300

    refactor: application repository layer

--- src/apps/auth/usecases/repos/get-auth-user-by-email-repository.ts
+++
@@ -1,9 +0,0 @@
-import { AuthUser } from '@/domains/auth/entities';
-
-export interface IGetAuthUserByEmailRepository {
-  get(email: string): IGetAuthUserByEmailRepository.Result;
-}
-
-export namespace IGetAuthUserByEmailRepository {
-  export type Result = Promise<AuthUser | null>;
-}
