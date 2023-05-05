7e5202895c7f175715e682ffc55cb391afaf0287
Arthur Sarmento <85639170+arthurbs17@users.noreply.github.com>
2023-05-05 15:16:00 -0300

    Feat/apply rate limit in nest boilerpate (#42)
    * feat: rate limit done
    
    * chore: remove test route
    
    * chore: remove .env do repositório e add env.example

--- src/apps/api-users/infra/nest/modules/api_users.module.ts
+++ src/apps/api-users/infra/nest/modules/api_users.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PrismaModule } from '@/libs/prisma';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { NotificationsModule } from '@/apps/notifications/infra';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from './users.module';
 import { AuthModule } from './auth.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { IORedisConfig } from '@/libs/ioredis';
+import { ThrottlerStorageRedisService } from 'nestjs-throttler-storage-redis';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -16,6 +20,22 @@ import { AuthModule } from './auth.module';
     NotificationsModule,
     AuthModule,
     UsersModule,
+    ThrottlerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<IORedisConfig>) => ({
+        ttl: 60,
+        limit: 100,
+        storage: new ThrottlerStorageRedisService(
+          configService.get<string>('APP_REDIS_URL'),
+        ),
+      }),
+    }),
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
   ],
 })
 export class ApiUsersModule {}
